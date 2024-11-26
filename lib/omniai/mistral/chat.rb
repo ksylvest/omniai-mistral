0f46bd82aeeb728483fd96e1612d07e5ef8313f4
Kevin Sylvestre <kevin@ksylvest.com>
2024-11-26 09:42:38 -0800

    Define new models
    This defines the full set of available models for mistral. It keeps the SMALL + MEDIUM + LARGE constants around for legacy support.

--- lib/omniai/mistral/chat.rb
+++ lib/omniai/mistral/chat.rb
@@ -13,10 +13,26 @@ module OmniAI
     #   chat.completion([{ role: 'system', content: 'Tell me a joke.' }])
     class Chat < OmniAI::Chat
       module Model
-        SMALL = 'mistral-small-latest'
-        MEDIUM = 'mistral-medium-latest'
-        LARGE = 'mistral-large-latest'
-        CODESTRAL = 'codestral-latest'
+        MISTRAL_SMALL_2409 = 'mistral-small-2409'
+        MISTRAL_SMALL_LATEST = 'mistral-small-latest' # LEGACY
+        MISTRAL_MEDIUM_2312 = 'mistral-medium-2312'
+        MISTRAL_MEDIUM_LATEST = 'mistral-medium-latest'
+        MISTRAL_LARGE_2411 = 'mistral-large-2411'
+        MISTRAL_LARGE_LATEST = 'mistral-large-latest'
+        CODESTRAL_2405 = 'codestral-2405'
+        CODESTRAL_LATEST = 'codestral-latest'
+        MINISTRAL_3B_2410 = 'ministral-3b-2410'
+        MINISTRAL_3B_LATEST = 'ministral-3b-latest'
+        MINISTRAL_8B_2410 = 'ministral-8b-2410'
+        MINISTRAL_8B_LATEST = 'ministral-8b-latest'
+        MISTRAL_MODERATION_2411 = 'mistral-moderation-2411'
+        MISTRAL_MODERATION_LATEST = 'mistral-moderation-latest'
+        MISTRAL_EMBED = 'mistral-embed'
+        PIXTRAL_LARGE_2411 = 'pixtral-large-2411'
+        PIXTRAL_LARGE_LATEST = 'pixtral-large-latest'
+        SMALL = MISTRAL_SMALL_LATEST
+        MEDIUM = MISTRAL_MEDIUM_LATEST # LEGACY
+        LARGE = MISTRAL_LARGE_LATEST
       end
 
       DEFAULT_MODEL = Model::LARGE
