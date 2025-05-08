2a5efc62d6b618f2636f8342d335f6f0bb846a28
Kevin Sylvestre <kevin@ksylvest.com>
2025-05-08 12:27:52 -0700

    Schema response-format support

--- lib/omniai/mistral/chat.rb
+++ lib/omniai/mistral/chat.rb
@@ -12,13 +12,22 @@ module OmniAI
     #   chat.completion({ role: 'user', content: 'Tell me a joke.' })
     #   chat.completion([{ role: 'system', content: 'Tell me a joke.' }])
     class Chat < OmniAI::Chat
+      module ResponseFormat
+        TEXT_TYPE = "text"
+        JSON_TYPE = "json_object"
+        SCHEMA_TYPE = "json_schema"
+      end
+
       module Model
         MISTRAL_SMALL_2402 = "mistral-small-2402" # LEGACY
         MISTRAL_SMALL_2409 = "mistral-small-2409" # LEGACY
-        MISTRAL_SMALL_2501 = "mistral-small-2501"
+        MISTRAL_SMALL_2501 = "mistral-small-2501" # LEGACY
+        MISTRAL_SMALL_2503 = "mistral-small-2503" # LEGACY
         MISTRAL_SMALL_LATEST = "mistral-small-latest"
         MISTRAL_MEDIUM_2312 = "mistral-medium-2312" # LEGACY
-        MISTRAL_MEDIUM_LATEST = "mistral-medium-latest" # LEGACY
+        MISTRAL_MEDIUM_2505 = "mistral-medium-2505"
+        MISTRAL_MEDIUM_LATEST = "mistral-medium-latest"
+        MISTRLA_LARGE_2402 = "mistral-large-2402" # LEGACY
         MISTRAL_LARGE_2411 = "mistral-large-2411"
         MISTRAL_LARGE_LATEST = "mistral-large-latest"
         CODESTRAL_2405 = "codestral-2405"
@@ -33,21 +42,19 @@ module OmniAI
         PIXTRAL_LARGE_2411 = "pixtral-large-2411"
         PIXTRAL_LARGE_LATEST = "pixtral-large-latest"
         SMALL = MISTRAL_SMALL_LATEST
-        MEDIUM = MISTRAL_MEDIUM_LATEST # LEGACY
+        MEDIUM = MISTRAL_MEDIUM_LATEST
         LARGE = MISTRAL_LARGE_LATEST
         PIXTRAL = PIXTRAL_LARGE_LATEST
         CODESTRAL = CODESTRAL_LATEST
       end
 
-      DEFAULT_MODEL = Model::PIXTRAL_LARGE_LATEST
+      DEFAULT_MODEL = Model::MISTRAL_MEDIUM_LATEST
 
       module Role
         ASSISTANT = "assistant"
         USER = "user"
         SYSTEM = "system"
       end
-
-      JSON_RESPONSE_FORMAT = { type: "json_object" }.freeze
 
     protected
 
@@ -58,7 +65,7 @@ module OmniAI
           model: @model,
           stream: stream? || nil,
           temperature: @temperature,
-          response_format: (JSON_RESPONSE_FORMAT if @format.eql?(:json)),
+          response_format:,
           tools: (@tools.map(&:serialize) if @tools&.any?),
         }).compact
       end
@@ -66,6 +73,20 @@ module OmniAI
       # @return [String]
       def path
         "/#{OmniAI::Mistral::Client::VERSION}/chat/completions"
+      end
+
+      # @raise [ArgumentError]
+      #
+      # @return [Hash, nil]
+      def response_format
+        return if @format.nil?
+
+        case @format
+        when :text then { type: ResponseFormat::TEXT_TYPE }
+        when :json then { type: ResponseFormat::JSON_TYPE }
+        when OmniAI::Schema::Format then { type: ResponseFormat::SCHEMA_TYPE, json_schema: @format.serialize }
+        else raise ArgumentError, "unknown format=#{@format}"
+        end
       end
     end
   end
